62be5a6136c3b60dcd3153a5bb3770f805a0e547
Eduardo Marçal <edumarcalnet@gmail.com>
2020-01-11 17:06:31 -0300

    Updates

---
+++ precache-manifest.74e5bd9c421b0da17f628f2a4b370a1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f36b083af1b2681b5455a7dd0f12e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "899d0986389345153333",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bde12b30405d41d18cc8",
+    "url": "/static/js/2.d448b3b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d448b3b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "899d0986389345153333",
+    "url": "/static/js/main.a6816389.chunk.js"
+  },
+  {
+    "revision": "e8ae05c32d48ae330e78",
+    "url": "/static/js/runtime-main.1b122566.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
