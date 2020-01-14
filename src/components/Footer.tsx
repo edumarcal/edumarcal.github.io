20f2628ac414b931552bbf33a656349c8d33da09
Eduardo Marçal <edumarcalnet@gmail.com>
2020-01-14 18:40:40 -0300

    :art: Imporove Footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Footer: React.FC = () => {
-  return <>Footer</>;
+  const { t } = useTranslation();
+  return (
+    <>
+      <h3 className="App-footer-title">
+        {t("footer.title")}{" "}
+        <span className="emoji-mart-emoji">
+          <span className="App-footer-heart"></span>
+        </span>{" "}
+        {t("footer.by")} Eduardo Marçal
+      </h3>
+      <p className="App-footer-subtitle">
+        {t("footer.wrong")}{" "}
+        <a
+          href="https://github.com/edumarcal/edumarcal.github.io/issues/new"
+          className="App-link"
+        >
+          {" "}
+          {t("footer.here")}
+        </a>
+      </p>
+    </>
+  );
 };
 
 export default Footer;
