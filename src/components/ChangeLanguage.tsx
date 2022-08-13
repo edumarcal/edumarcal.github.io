668c99b383817cd4fba573d48a6c0f6165a93b23
Eduardo Marçal Oliveira <edumarcalnet@gmail.com>
2022-08-13 03:07:12 -0300

    :bug: fix style change language

--- src/components/ChangeLanguage.tsx
+++ src/components/ChangeLanguage.tsx
@@ -1,36 +1,36 @@
 import i18next from "i18next";
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const ChangeLanguage: React.FC = () => {
+  const [language, setLanguage] = useState("en");
   const handleChangeLanguage = (value: string) => (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     e.preventDefault();
+    setLanguage(value);
     i18next.changeLanguage(value);
   };
 
-  const navigatorLanguage = navigator.language;
+  const isActiveEnLanguage = () => {
+    return language === "en" ? "-active" : "";
+  };
 
-  const isActiveLanguage = (lng: string): string => {
-    if (lng === "pt" && navigatorLanguage === "pt-BR") {
-      return "-active";
-    }
-
-    return i18next.language === lng ? "-active" : "";
+  const isActivePtBrLanguage = () => {
+    return language === "pt" || language === "pt-BR" ? "-active" : "";
   };
 
   const { t } = useTranslation();
   return (
     <>
       <div
-        className={`App-header-menu-button${isActiveLanguage("en")}`}
+        className={`App-header-menu-button${isActiveEnLanguage()}`}
         onClick={handleChangeLanguage("en")}
       >
         {t("language.english")}
       </div>{" "}
       <div
-        className={`App-header-menu-button${isActiveLanguage("pt")}`}
+        className={`App-header-menu-button${isActivePtBrLanguage()}`}
         onClick={handleChangeLanguage("pt")}
       >
         {t("language.portuguese")}
