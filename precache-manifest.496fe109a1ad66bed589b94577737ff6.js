cf130ee69c226dee1b0f3dd7ffa2ca11b90c43fd
Eduardo Marçal Oliveira <edumarcalnet@gmail.com>
2022-08-16 22:12:54 -0300

    🎉 Improve  (#5) (#6)
    * :bento: Updates (#2) (#4)
    
    * Updates

---
+++ precache-manifest.496fe109a1ad66bed589b94577737ff6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806c759b185dcd0afba89d3ea25af5c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ffa03bd5c1517b7137a",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "ae688edbebaee74153cf",
+    "url": "/static/css/main.11006785.chunk.css"
+  },
+  {
+    "revision": "8ffa03bd5c1517b7137a",
+    "url": "/static/js/2.9733f0b0.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/static/js/2.9733f0b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae688edbebaee74153cf",
+    "url": "/static/js/main.77a29ed1.chunk.js"
+  },
+  {
+    "revision": "e8ae05c32d48ae330e78",
+    "url": "/static/js/runtime-main.1b122566.js"
+  },
+  {
+    "revision": "305b24d3e0d9687034112984e57cf48c",
+    "url": "/static/media/actions-icon-actions.305b24d3.svg"
+  },
+  {
+    "revision": "67a595d5916debc40f2a03aeb1452bc1",
+    "url": "/static/media/cypress.67a595d5.svg"
+  },
+  {
+    "revision": "e45d0e6d9404119d9158a55cd399dedf",
+    "url": "/static/media/white-ms.e45d0e6d.svg"
+  }
+]);
